bfb16ff85cb8c8bd3a0d50801f5349c2a44f9649
abdulraahmansaleh <73913934+abdulraahmansaleh@users.noreply.github.com>
2020-11-08 23:38:17 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Enter the number of dollars\n");
+    long long a;
+    scanf("%x",&a);
+    printf("%x dollars = %x Egyptian pounds",a,a*16);
+    return 0;
+}
